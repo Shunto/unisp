25c216f927b03fed8f64b1752693454ba8065c50
Naoki <naoki826divide@gmail.com>
2016-11-13 08:20:08 +0000

    attendance

--- unisp/app/models/attendance.rb
+++ unisp/app/models/attendance.rb
@@ -1,3 +1,3 @@
 class Attendance < ApplicationRecord
-    belongs_to :content
+belongs_to :subject
 end
--- unisp/app/models/subject.rb
+++ unisp/app/models/subject.rb
@@ -1,4 +1,5 @@
 class Subject < ApplicationRecord
     has_many :contents
     has_many :reports
+    has_many :attendances
 end
